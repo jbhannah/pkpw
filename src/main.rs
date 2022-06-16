9b9b2534d155fd257869ac4562b540b39d0d1467
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2022-06-16 12:16:19 -0700

    Add -n option (fixes #1)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use lazy_static::lazy_static;
 use rand::{prelude::SliceRandom, thread_rng, Rng};
 
@@ -7,18 +8,28 @@ lazy_static! {
         include_str!("../pokemon.txt").trim().split("\n").collect();
 }
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Number of Pokémon names to use in the generated password.
+    #[clap(short = 'n', value_parser, default_value_t = 4)]
+    count: usize,
+}
+
 /// Print a password of four random Pokémon names joined by a space character.
 fn main() {
-    let password = pick(&mut thread_rng()).join(" ");
+    let args = Args::parse();
+
+    let password = pick(args.count, &mut thread_rng()).join(" ");
 
     println!("{}", password);
 }
 
-/// Pick four random Pokémon names using the given RNG.
-fn pick<R: Rng + ?Sized>(rng: &mut R) -> Vec<&str> {
+/// Pick a number of random Pokémon names using the given RNG.
+fn pick<R: Rng + ?Sized>(count: usize, rng: &mut R) -> Vec<&str> {
     let mut pokemon = POKEMON.clone();
     pokemon.shuffle(rng);
-    pokemon.into_iter().take(4).collect()
+    pokemon.into_iter().take(count).collect()
 }
 
 #[cfg(test)]
@@ -27,11 +38,11 @@ mod test {
 
     use super::*;
 
-    /// Ensure that pick() returns a vector of four strings.
+    /// Ensure that pick(4, …) returns a vector of four strings.
     #[test]
     fn test_pick() {
         let mut rng = rand::rngs::StdRng::seed_from_u64(913);
-        let pokemon = pick(&mut rng);
+        let pokemon = pick(4, &mut rng);
 
         assert_eq!(
             vec!["Shroomish", "Venusaur", "Froakie", "Tyranitar"],
@@ -43,10 +54,10 @@ mod test {
     #[test]
     fn test_pick_rand() {
         let mut rng_1 = rand::rngs::StdRng::seed_from_u64(913);
-        let pokemon_1 = pick(&mut rng_1);
+        let pokemon_1 = pick(4, &mut rng_1);
 
         let mut rng_2 = rand::rngs::StdRng::seed_from_u64(319);
-        let pokemon_2 = pick(&mut rng_2);
+        let pokemon_2 = pick(4, &mut rng_2);
 
         assert_ne!(pokemon_1, pokemon_2);
     }
