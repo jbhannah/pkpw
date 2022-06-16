68d5fd02d4d01f36bc76ac7ab27fd8033f091c6b
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2022-06-16 11:21:55 -0700

    Decompose main()

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use rand::{prelude::SliceRandom, thread_rng};
+use lazy_static::lazy_static;
+use rand::{prelude::SliceRandom, thread_rng, Rng};
 
 fn main() {
-    let mut pokemon = include_str!("../pokemon.txt")
-        .trim()
-        .split("\n")
-        .collect::<Vec<&str>>();
-
-    pokemon.shuffle(&mut thread_rng());
-
-    let password = pokemon.into_iter().take(4).collect::<Vec<&str>>().join(" ");
+    let password = pick(&mut thread_rng()).join(" ");
 
     println!("{}", password);
+}
+
+fn pick<R: Rng + ?Sized>(rng: &mut R) -> Vec<&str> {
+    let mut pokemon = POKEMON.clone();
+    pokemon.shuffle(rng);
+    pokemon.into_iter().take(4).collect()
+}
+
+lazy_static! {
+    pub static ref POKEMON: Vec<&'static str> =
+        include_str!("../pokemon.txt").trim().split("\n").collect();
 }
