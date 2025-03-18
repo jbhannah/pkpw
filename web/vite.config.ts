2a88fb658047da933a8dea7b0f4566d0c456a236
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-03-18 16:01:08 -0700

    Add Tailwind CSS and daisyUI

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,10 @@
 import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import topLevelAwait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact(), wasm(), topLevelAwait()],
+  plugins: [preact(), wasm(), topLevelAwait(), tailwindcss()],
 });
