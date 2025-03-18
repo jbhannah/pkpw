ee94d9b4250f07f18d0d2f555945bd8882d88d09
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-03-18 15:48:53 -0700

    Add pkpw and Vite wasm support

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,9 @@
 import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), wasm(), topLevelAwait()],
 });
