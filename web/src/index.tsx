1433ae13228b10ee56d1e839d5b2e47603ba42e3
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-03-18 21:42:07 -0700

    Add theme toggle

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,18 +1,21 @@
 import { render } from "preact";
 
 import { Password } from "./components/password";
+import { ThemeToggle } from "./components/theme-toggle";
 import "./style.css";
 
 export function App() {
   return (
     <div class="min-h-screen w-full flex flex-col">
-      <div>Header</div>
+      <header>
+        <ThemeToggle />
+      </header>
       <div class="flex-1 flex flex-col items-center justify-center p-4">
         <h1>pkpw</h1>
         <p>What if correct horse battery staple, but Pokémon.</p>
         <Password />
       </div>
-      <div>Footer</div>
+      <footer>Footer</footer>
     </div>
   );
 }
