6fa84a8cbbb3b3c8efb6b85b7ec982a1d8c750c1
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-03-19 11:17:09 -0700

    Count password generation events

--- web/src/components/password.tsx
+++ web/src/components/password.tsx
@@ -1,8 +1,16 @@
 import { useSignal } from "@preact/signals";
 import { pkpw } from "pkpw";
+import { usePostHog } from "posthog-js/react";
 
 export function Password() {
-  const password = useSignal(pkpw());
+  const posthog = usePostHog();
+
+  const generatePassword = () => {
+    posthog.capture("generated_password");
+    return pkpw();
+  };
+
+  const password = useSignal(generatePassword());
 
   return (
     <div class="w-full flex flex-col items-center gap-y-4">
@@ -27,7 +35,7 @@ export function Password() {
         <button
           type="button"
           class="btn btn-primary"
-          onClick={() => (password.value = pkpw())}
+          onClick={() => (password.value = generatePassword())}
         >
           Generate
         </button>
