f3a9c5ad124e740264ea25b84542618e40e6fec5
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-03-18 23:03:50 -0700

    Rename wasm-exported function to pkpw

--- src/wasm.rs
+++ src/wasm.rs
@@ -4,11 +4,7 @@ use wasm_bindgen::prelude::*;
 use crate::generate;
 
 #[wasm_bindgen]
-pub fn generate_password(
-    len: Option<usize>,
-    count: Option<usize>,
-    separator: Option<String>,
-) -> String {
+pub fn pkpw(len: Option<usize>, count: Option<usize>, separator: Option<String>) -> String {
     let mut rng = rng();
 
     generate(
@@ -24,16 +20,16 @@ mod test {
     use crate::{DIGITS, SPECIAL};
     use wasm_bindgen_test::*;
 
-    use crate::wasm::generate_password;
+    use crate::wasm::pkpw;
 
     #[wasm_bindgen_test]
     fn returns_string() {
-        assert!(!generate_password(None, None, None).is_empty());
+        assert!(!pkpw(None, None, None).is_empty());
     }
 
     #[wasm_bindgen_test]
     fn accepts_length() {
-        let password = generate_password(Some(50), None, None);
+        let password = pkpw(Some(50), None, None);
         let len = password.len();
 
         assert!(len >= 50);
@@ -41,7 +37,7 @@ mod test {
 
     #[wasm_bindgen_test]
     fn accepts_count() {
-        let password = generate_password(None, Some(6), None);
+        let password = pkpw(None, Some(6), None);
         let words = password.split(' ').count();
 
         assert!(words >= 6); // some pokémon names have spaces in them
@@ -49,7 +45,7 @@ mod test {
 
     #[wasm_bindgen_test]
     fn accepts_digit_split() {
-        let password = generate_password(None, None, Some("digit".to_string()));
+        let password = pkpw(None, None, Some("digit".to_string()));
         let words = password.split(&DIGITS[..]).count();
 
         assert!(words >= 3); // Porygon2
@@ -57,7 +53,7 @@ mod test {
 
     #[wasm_bindgen_test]
     fn accepts_special_split() {
-        let password = generate_password(None, None, Some("special".to_string()));
+        let password = pkpw(None, None, Some("special".to_string()));
         let words = password.split(&SPECIAL[..]).count();
 
         assert!(words >= 3);
@@ -65,7 +61,7 @@ mod test {
 
     #[wasm_bindgen_test]
     fn accepts_chararcter_split() {
-        let password = generate_password(None, None, Some("%".to_string()));
+        let password = pkpw(None, None, Some("%".to_string()));
         let char_count = password.chars().filter(|c| c == &'%').count();
 
         assert_eq!(3, char_count);
