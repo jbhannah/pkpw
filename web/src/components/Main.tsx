ad0d792a86207d2266da8ea8868365a44c319e0e
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-03-20 13:19:28 -0700

    Extract main component

---
+++ web/src/components/Main.tsx
@@ -0,0 +1,57 @@
+import { PasswordBox } from "./PasswordBox";
+
+export const Main = () => (
+  <main class="flex flex-col items-center mt-8 p-4">
+    <h1 class="text-6xl mb-6 text-center">pkpw</h1>
+    <h2 class="text-xl mb-6 text-center max-w-xl">
+      What if{" "}
+      <a
+        class="link link-accent"
+        href="https://xkcd.com/936/"
+        target="_blank"
+        rel="noopener"
+      >
+        correct horse battery staple
+      </a>
+      {", "}but Pokémon.
+    </h2>
+    <div class="w-full max-w-lg mt-8 mb-16">
+      <PasswordBox />
+    </div>
+    <p class="max-w-2xl text-center mb-4">
+      A password generator that uses Pokémon names to generate strong passwords.{" "}
+      <a
+        class="link link-accent"
+        href="https://github.com/jbhannah/pkpw"
+        target="_blank"
+        rel="noopener"
+      >
+        Learn more
+      </a>
+    </p>
+    <p class="max-w-2xl text-center mb-4">
+      All Pokémon names are ™ and ©{" "}
+      <a
+        class="link link-accent"
+        href="https://www.pokemon.com"
+        target="_blank"
+        rel="noopener"
+      >
+        The Pokémon Company
+      </a>
+      {"."}
+    </p>
+    <p class="max-w-2xl text-center">
+      Created by{" "}
+      <a
+        class="link link-accent"
+        href="https://jbhannah.net"
+        target="_blank"
+        rel="noopener"
+      >
+        Jesse Brooklyn Hannah
+      </a>
+      {"."}
+    </p>
+  </main>
+);
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,7 +3,7 @@ import { render } from "preact";
 import { PostHogProvider } from "posthog-js/react";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
-import { PasswordBox } from "./components/PasswordBox";
+import { Main } from "./components/Main";
 import "./style.css";
 
 const options = {
@@ -17,62 +17,9 @@ const App = () => (
   <PostHogProvider apiKey={REACT_APP_PUBLIC_POSTHOG_KEY} options={options}>
     <div class="min-h-screen w-full flex flex-col">
       <Header />
-      <main class="flex-1">
-        <div class="flex flex-col items-center mt-8 p-4">
-          <h1 class="text-6xl mb-6 text-center">pkpw</h1>
-          <h2 class="text-xl mb-6 text-center max-w-xl">
-            What if{" "}
-            <a
-              class="link link-accent"
-              href="https://xkcd.com/936/"
-              target="_blank"
-              rel="noopener"
-            >
-              correct horse battery staple
-            </a>
-            {", "}but Pokémon.
-          </h2>
-          <div class="w-full max-w-lg mt-8 mb-16">
-            <PasswordBox />
-          </div>
-          <p class="max-w-2xl text-center mb-4">
-            A password generator that uses Pokémon names to generate strong
-            passwords.{" "}
-            <a
-              class="link link-accent"
-              href="https://github.com/jbhannah/pkpw"
-              target="_blank"
-              rel="noopener"
-            >
-              Learn more
-            </a>
-          </p>
-          <p class="max-w-2xl text-center mb-4">
-            All Pokémon names are ™ and ©{" "}
-            <a
-              class="link link-accent"
-              href="https://www.pokemon.com"
-              target="_blank"
-              rel="noopener"
-            >
-              The Pokémon Company
-            </a>
-            {"."}
-          </p>
-          <p class="max-w-2xl text-center">
-            Created by{" "}
-            <a
-              class="link link-accent"
-              href="https://jbhannah.net"
-              target="_blank"
-              rel="noopener"
-            >
-              Jesse Brooklyn Hannah
-            </a>
-            {"."}
-          </p>
-        </div>
-      </main>
+      <div class="flex-1">
+        <Main />
+      </div>
       <Footer />
     </div>
   </PostHogProvider>
