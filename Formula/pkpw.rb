23343257f8487652a5da66ea66e79801fa8c3dcc
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-06-04 12:29:19 -0700

    fix formula description

--- Formula/pkpw.rb
+++ Formula/pkpw.rb
@@ -1,5 +1,5 @@
 class Pkpw < Formula
-  desc "What if correct horse battery staple, but Pokémon."
+  desc "What if correct horse battery staple, but Pokémon"
   homepage "https://pkpw.jbhannah.net"
   url "https://github.com/jbhannah/pkpw/archive/refs/tags/v1.3.2.tar.gz"
   sha256 "15d58b892bb5e7cef96871b9f8d6619662183f6fa0b35f38a6eea270f3b4d0a6"
